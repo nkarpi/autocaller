8e25fe487565635b9acdfa4aac2386e54b57636d
Nolan Karpinski <nkarpi@Nolans-MacBook-Pro.local>
2015-10-16 20:36:15 -0400

    restructured repo

--- autocaller/make_call.py
+++ autocaller/make_call.py
@@ -1,6 +1,7 @@
 # Download the library from twilio.com/docs/libraries
 from twilio.rest import TwilioRestClient
 import praw
+import tokens
 
 # app = Flask(__name__)
  
@@ -12,6 +13,7 @@ import praw
 # call = client.calls.create(to="+14085551234",  # call this number
 #                            from_="+12125551234", # call from this number
 #                            url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
+
 r = praw.Reddit(user_agent='web:autocaller:v1.0 (by nkarpi)')
 
 posts_generator = r.get_content('https://www.reddit.com/')
