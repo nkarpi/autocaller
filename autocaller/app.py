fe34753f54ffb140ebc09e1b18d4eda72dd6e0c7
Nolan Karpinski <nkarpi@Nolans-MacBook-Pro.local>
2015-10-17 14:49:03 -0400

    added the /Call endpoint

--- autocaller/app.py
+++ autocaller/app.py
@@ -20,20 +20,20 @@ app.config.from_pyfile('settings.py')
 #     return str("Hello World foobar")
 
 # Voice Request URL
-# @app.route('/call', methods=['POST'])
-# def call():
-#     # Get phone number we need to call
+@app.route('/call', methods=['POST'])
+def call():
+    # Get phone number we need to call
 
-#     twilio_client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    twilio_client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     
-#     twilio_client.calls.create(from_=TWILIO_NUMBER,
-#                                to=TO_CALL_NUMBER,
-#                                url=url_for('.outbound',
-#                                            _external=True))
+    twilio_client.calls.create(from_=TWILIO_NUMBER,
+                               to=CALL_TO_NUMBER,
+                               url=url_for('.message',
+                                           _external=True))
 
 
-@app.route('/outbound', methods=['GET'])
-def outbound():
+@app.route('/message', methods=['POST'])
+def message():
 
     response = twiml.Response()
 
@@ -42,7 +42,7 @@ def outbound():
     posts = list(posts_generator)
     top_post = posts[0]
 
-    response.say("Nolan Karpinski " + str(top_post),
+    response.say("Nolan Karpinski " + str(top_post).split('::')[1],
                  voice='alice')
 
     return str(response)
