92d655aa7ad5876da7dbb1cd45c7ccbd6111e959
Nolan Karpinski <nkarpi@Nolans-MacBook-Pro.local>
2015-10-19 00:02:13 -0400

    changed message endpoint

--- voice.py
+++ voice.py
@@ -1,25 +1,32 @@
+# import flask - lightweight web framework
 from flask import Flask
+# import twiml - a twilio app for making the voice message
 import twilio.twiml
+# import praw - Python Reddit API Wrapper
 import praw
  
 app = Flask(__name__)
 
 REDDIT_URL='https://www.reddit.com/'
  
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/message", methods=['GET', 'POST'])
 def message():
 
-    resp = twilio.twiml.Response()
-
+	# instantiate a praw object and retrieve the top posts
+	# parse the top posts to get the #1 headline w/out associated score
     r = praw.Reddit(user_agent='web:autocaller:v1.0 (by nkarpi)')
     posts_generator = r.get_content(REDDIT_URL)
-    posts = list(posts_generator)
-    top_post = posts[0]
+    top_post = list(posts_generator)[0]
+    headline = str(top_post).split('::')[1]
 
-    resp.say("Nolan Karpinski " + str(top_post).split('::')[1],
+    # instantiate a twiml respoonse obj. 
+    # Use the "say" verb to create XLM response
+    resp = twilio.twiml.Response()
+    resp.say("Hi Allie, happy to be here, you're hot",
                  voice='alice')
 
     return str(resp)
  
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=4000)
