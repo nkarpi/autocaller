f4d1422f1956d27ba569f13febeb4817ee7cb7ee
Nolan Karpinski <nkarpi@resnet154-225.medford.tufts.edu>
2015-10-20 12:38:31 -0400

    top level wrapper commenting

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# top level wrapper to run the autocaller app
 import os
 
 from autocaller.app import app
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    if port == 5000:
+    port = int(os.environ.get("PORT", 3000))
+    if port == 3000:
         app.debug = True
     app.run(host='0.0.0.0', port=port)
